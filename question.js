ea6418925e0f240d1eb3759c2531969c98c2c690
Feryellamouchi19 <124433281+Feryellamouchi19@users.noreply.github.com>
2024-03-29 16:56:58 +0100

    Create question.js

---
+++ question.js
@@ -0,0 +1,56 @@
+let questions = [
+    {
+        numb:1,
+        question:"What does HTML stand for?",
+        answer:"A. Hyper Text Markup Language",
+        Option:[
+            "A. Hyper Text Markup Language" ,
+            "B. High Tech Modular Language",
+            "C. Hyperlink and Text Markup Language"
+
+        ]
+    },
+    {
+        numb:2,
+        question:"Which HTML5 tag is used for creating hyperlinks?",
+        answer:"B. a",
+        Option:[
+            "A. link",
+            "B. a",
+            "C. ln",
+        ]
+    },
+    {
+        numb:3,
+        question:"What is the purpose of the < header > tag in HTML5?",
+        answer:"B.Represents a header for a document or a section ",
+        Option:[
+            "A. Defines a header cell in a table",
+            "B.Represents a header for a document or a section ", 
+            "C. Specifies a horizontal line in the document"
+            
+        ]
+    },
+    {
+        numb:4,
+        question:"Which HTML5 tag is used to embed a video on a web page?",
+        answer:"A.< video > ",
+        Option:[
+            "A.< video > ", 
+            "B.< media >",
+            "C.< embed >"
+            
+        ]
+    },
+    {
+        numb:5,
+        question:" What does the acronym 'CSS' stand for in the context of web development?",
+        answer:"C. Cascading Style Sheets ",
+        Option:[
+            "A. Computer Style Sheets",
+            "B. Creative Style Sheets",
+            "C. Cascading Style Sheets "
+            
+        ]
+    }
+]
